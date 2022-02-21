24ba6dfa0d79cc59aac4742083fec1dd5f2bb76f
maymonyu <yuvalmaymon30@gmail.com>
2022-02-21 21:03:27 +0200

    prev_next_median

--- seinfeld_noise_fixing.py
+++ seinfeld_noise_fixing.py
@@ -113,55 +113,23 @@ def display_bilateral_filter_result(frames: np.ndarray, filter_size=-1) -> np.nd
   return fixed_frames
 
 
-def display_following_frame_filter_results(frames: np.ndarray, black_threshold=50, white_threshold=200) -> None:
-  minimal_blackish_or_whitish_channels = 3
-  H, W, _ = frames[0].shape
+def display_prev_next_median_filter_result(frames: np.ndarray) -> np.ndarray:
   # display_images_as_video(frames)
   
+  number_of_prev_and_next_filters = 2
   fixed_frames = np.zeros_like(frames)
-  for idx, frame in enumerate(frames):
-    next_frame_index = idx + 1
-    if next_frame_index is len(frames):
-      break
 
-    frame_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-
-    dark_pixels_mask = frame_hsv[:, :, 2] < black_threshold
-
-    bright_pixels_mask = frame_hsv[:, :, 2] > white_threshold
-
-    dark_and_bright_mask = dark_pixels_mask | bright_pixels_mask
-    mask = dark_and_bright_mask
-
-    # blackish_pixels_mask = frame < black_threshold
-    # number_of_blackish_channels_in_pixel = np.count_nonzero(blackish_pixels_mask, axis=2)
-    # minimal_blackish_pixels_mask = number_of_blackish_channels_in_pixel >= minimal_blackish_or_whitish_channels
-    #
-    # whitish_pixels_mask = frame > white_threshold
-    # number_of_whitish_channels_in_pixel = np.count_nonzero(whitish_pixels_mask, axis=2)
-    # minimal_whitish_pixels_mask = number_of_whitish_channels_in_pixel >= minimal_blackish_or_whitish_channels
-    # blackish_and_whitish_mask = minimal_blackish_pixels_mask | minimal_whitish_pixels_mask
-   
-    # mask = blackish_and_whitish_mask
-
-    masked_frame = np.copy(frame)
-    masked_frame[mask] = 255, 255, 255
-    masked_frame[~mask] = 0, 0, 0
-   
-    image = get_side_by_side_image(frame, masked_frame, is_horizontal=False)
-    display_image(image)
+  for idx in range(number_of_prev_and_next_filters, len(frames)-number_of_prev_and_next_filters-1):
+    current_layer = frames[idx - number_of_prev_and_next_filters: idx + number_of_prev_and_next_filters + 1]
+    median = np.median(current_layer, axis=0)
+    fixed_frame = median
+    fixed_frames[idx] = fixed_frame
     
-    # next_frame = frames[next_frame_index]
-    #
-    # fixed_frame = np.copy(frame)
-    # fixed_frame[mask] = next_frame[mask]
-    #
-    # fixed_frames[idx] = fixed_frame
-    #
     # image = get_side_by_side_image(frame, fixed_frame, is_horizontal=False)
     # display_image(image)
-    #
-  # display_images_as_video(fixed_frames)
+  
+  # display_side_by_side_video(frames, fixed_frames, is_horizontal=False)
+  return fixed_frames
 
 
 def release_resources(video_capture: cv.VideoCapture):
@@ -171,7 +139,7 @@ def release_resources(video_capture: cv.VideoCapture):
 
 if __name__ == '__main__':
   frames, video_capture = load_video()
-  first_frames = frames[:5]
+  first_frames = frames[:200]
   
   _, H, W, _ = first_frames.shape
   
@@ -179,16 +147,16 @@ if __name__ == '__main__':
 
   fixed_frames = original_frames
   # fixed_frames = display_gaussian_filter_result(fixed_frames)
-  fixed_frames = display_bilateral_filter_result(fixed_frames)
-  fixed_frames = display_bilateral_filter_result(fixed_frames)
+  # fixed_frames = display_bilateral_filter_result(fixed_frames)
   # fixed_frames = display_median_filter_result(fixed_frames, 3)
+  fixed_frames = display_prev_next_median_filter_result(fixed_frames)
   # display_mean_filter_result(fixed_frames)
   # display_following_frame_filter_results(fixed_frames)
   
-  display_side_by_side_video(original_frames, fixed_frames, wait_key=True)
-  # display_side_by_side_video(original_frames, fixed_frames)
+  # display_side_by_side_video(original_frames, fixed_frames, wait_key=True)
+  display_side_by_side_video(original_frames, fixed_frames)
   
-  cv.imwrite('Results/image.png', get_side_by_side_image(original_frames[0], fixed_frames[0],
-                                                                             is_horizontal=False))
+  output_image = get_side_by_side_image(original_frames[2], fixed_frames[2], is_horizontal=False)
+  cv.imwrite('Results/image.png', output_image)
   
   release_resources(video_capture)
